c43b713162bbdc3f1232b827ae2d1d0ce7537cd6
Ashwin Gatadi <ashwin.gatadi@gmail.com>
2017-04-01 21:50:35 -0700

    Socket Connection basics

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 var app = express();
 var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('New User Connected');
+
+    socket.on('disconnect',()=>{
+        console.log('User disconnected');
+    });
+});
 
 var publicPath = path.join(__dirname, '..','public');
 
 app.use(express.static(publicPath)); 
 
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`application started on port ${port}`)
 });
